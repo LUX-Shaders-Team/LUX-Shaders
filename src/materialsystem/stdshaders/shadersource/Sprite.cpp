86e4f06ceca49a15993d9a01556fbe9a0b66cad6
Dkxtro2 <Dave.Diedrichs@gmx.de>
2026-05-18 13:58:39 +0200

    Hardcode RenderMote_t on the SpriteShader
    I have my Reasons for doing this

--- src/materialsystem/stdshaders/shadersource/Sprite.cpp
+++ src/materialsystem/stdshaders/shadersource/Sprite.cpp
@@ -9,7 +9,22 @@
 #include "../cpp_lux_shared.h"
 
 // const.h has an enum for sprite Renderingmodes
-#include "const.h"
+enum RenderMode_t
+{
+	kRenderNormal = 0,		// src
+	kRenderTransColor,		// c*a+dest*(1-a)
+	kRenderTransTexture,	// src*a+dest*(1-a)
+	kRenderGlow,			// src*a+dest -- No Z buffer checks -- Fixed size in screen space
+	kRenderTransAlpha,		// src*srca+dest*(1-srca)
+	kRenderTransAdd,		// src*a+dest
+	kRenderEnvironmental,	// not drawn, used for environmental effects
+	kRenderTransAddFrameBlend, // use a fractional frame value to blend between animation frames
+	kRenderTransAlphaAdd,	// src + dest*(1-a)
+	kRenderWorldGlow,		// Same as kRenderGlow but not fixed size in screen space
+	kRenderNone,			// Don't render.
+
+	kRenderModeCount,		// must be last
+};
 
 // Includes for Shaderfiles...
 #include "lux_sprite_ps30.inc"
